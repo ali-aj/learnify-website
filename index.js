e6a961f466fe928a93c2983320deb8fdc5026ad3
Ali Mustafa <alij7478@gmail.com>
2024-03-17 17:08:31 +0500

    Added Sign In and Sign Up Pages

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.static('public'));
 app.use(express.json());
+
+app.get('/SignIn', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signIn.html'));
+});
+
+app.get('/SignUp', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signUp.html'));
+});
+
+app.get('/ForgetPassword', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'forgetPassword.html'));
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/SignIn');
+});
 
 const PORT = process.env.PORT || 3000;
 
---
+++ public/js/signUp.js
@@ -0,0 +1,36 @@
+function validatePassword() {
+    var password = document.getElementById("passwordField").value;
+    var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    var existingAlerts = document.querySelectorAll(".alert");
+
+    // Remove existing alerts
+    existingAlerts.forEach(function(alert) {
+        alert.parentNode.removeChild(alert);
+    });
+
+    var alertDiv = document.createElement("div");
+    if (!re.test(password)) {
+        alertDiv.className = "alert alert-danger";
+        alertDiv.role = "alert";
+        alertDiv.textContent = "Please choose a strong password!";
+        document.body.insertBefore(alertDiv, document.body.firstChild);
+        return false;
+    }
+    document.getElementById("signupform").reset();
+    alertDiv.className = "alert alert-success";
+    alertDiv.role = "alert";
+    alertDiv.textContent = "You have successfully Registered!";
+    document.body.insertBefore(alertDiv, document.body.firstChild);
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("showPassword").addEventListener("change", function () {
+        var passwordInput = document.getElementById("passwordField");
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+        } else {
+            passwordInput.type = "password";
+        }
+    });
+});
